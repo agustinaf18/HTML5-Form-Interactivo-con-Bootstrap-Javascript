a308777b255d2532683979c9f5d4466880ee3596
agustinaf18 <agussslp1529@gmail.com>
2023-01-04 20:08:32 +0000

    mensaje

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //agregamos un evento al elemento form, primero obtenemos el elemento
+  let form = document.querySelector("form");
+  //Luego manipulamos el elemento agregandole el evento submit
+  // form.addEventListener("evento",funcion que se va activar)
+  //on
+  form.addEventListener("submit", enviarDatos);
+
+  //declaramos la funcion que envia los datos del formulario
+  function enviarDatos(e) {
+    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpretado mi codigo
+    let card = document.getElementById("card");
+    let cvc = document.getElementById("cvc");
+    let amount = document.getElementById("amount");
+    let name = document.getElementById("name");
+    let lastName = document.getElementById("lastName");
+    let city = document.getElementById("city");
+    let postalCode = document.getElementById("postalcode");
+    let state = document.getElementById("state");
+    let message = document.getElementById("message");
+    let alert = document.getElementById("alert");
+
+    if (
+      card.value === "" ||
+      cvc.value === "" ||
+      amount.value === "" ||
+      name.value === "" ||
+      lastName.value === "" ||
+      city.value === "" ||
+      postalCode.value === "" ||
+      state.value === "" ||
+      message.value === ""
+    ) {
+      console.log("El campo email está vacio");
+      card.style.background = "rgb(242, 214, 214)";
+      cvc.style.background = "rgb(242, 214, 214)";
+      amount.style.background = "rgb(242, 214, 214)";
+      name.style.background = "rgb(242, 214, 214)";
+      lastName.style.background = "rgb(242, 214, 214)";
+      city.style.background = "rgb(242, 214, 214)";
+      postalCode.style.background = "rgb(242, 214, 214)";
+      state.style.background = "rgb(242, 214, 214)";
+      message.style.background = "rgb(242, 214, 214)";
+      alert.className = "alert alert-danger d-block";
+    }
+  }
 };
